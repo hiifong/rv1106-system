36e830a598c26c6a44aa64f7b714158fcf314b9e
hiifong <f@f.style>
2026-03-04 13:41:16 +0800

    Uncomment RK_PARTITION_CMD_IN_ENV for EMMC configuration

--- project/cfg/BoardConfig_IPC/BoardConfig-EMMC-NONE-RV1106_JETKVM_V2.mk
+++ project/cfg/BoardConfig_IPC/BoardConfig-EMMC-NONE-RV1106_JETKVM_V2.mk
@@ -52,6 +52,7 @@ export RK_BOOTARGS_CMA_SIZE="48M"
 
 # export RK_PARTITION_CMD_IN_ENV="32K(env),512K@32K(idblock),256K(uboot_a),256K(uboot_b),256K(misc),32M(boot_a),32M(boot_b),512M(system_a),512M(system_b),256M(oem),-(userdata)"
 # export RK_PARTITION_CMD_IN_ENV="32K(env),512K@32K(idblock),256K(uboot_a),256K(uboot_b),256K(misc),32M(boot_a),32M(boot_b),512M(system_a),512M(system_b),13640M(userdata)"
+
 export RK_PARTITION_CMD_IN_ENV="32K(env),512K@32K(idblock),256K(uboot_a),256K(uboot_b),256K(misc),32M(boot_a),32M(boot_b),512M(system_a),512M(system_b),-(userdata)"
 
 # config partition's filesystem type (squashfs is readonly)
